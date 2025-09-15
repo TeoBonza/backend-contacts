bd12fb94144d2fef3e80f213d740a85739da4df4
Teo <155564693+TeoBonza@users.noreply.github.com>
2025-09-15 13:59:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDb = require('./config/dbConnection');
 const setupSwagger = require('./swagger');
 const options = require('./swagger');
 const dotenv = require('dotenv').config();
+const rateLimiter = require('./middleware/rate-limiter');
 
 connectDb();
 const app = express();
@@ -11,6 +12,12 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
+
+// Applying rate limits
+app.use('/api/users/login', rateLimiter({ windowInSeconds: 60, maxRequests: 5 }));
+app.use('/api/users/register', rateLimiter({ windowInSeconds: 60, maxRequests: 5 }));
+app.use(rateLimiter({ windowInSeconds: 60, maxRequests: 100 }));
+
 app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorhandler);
