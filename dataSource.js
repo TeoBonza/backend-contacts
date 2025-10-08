a84260fbabe060301924111343e0cd31c3d0fa93
Teo <matteo@peculiarventures.com>
2025-10-08 17:06:12 +0300

    update dataSource file to fix the connection

--- dataSource.js
+++ dataSource.js
@@ -2,15 +2,14 @@ const { DataSource } = require('typeorm');
 const Contact = require('./models/contactModel');
 const User = require('./models/userModel');
 
+const config = JSON.parse(process.env.DATABASE_CONNECTION_OPTIONS);
+
 const AppDataSource = new DataSource({
-  type: 'mongodb',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 27017,
-  database: process.env.DB_NAME || 'mycontacts-backend',
+  ...config,
+  authSource: 'admin',
   synchronize: true,
   useUnifiedTopology: true,
   entities: [Contact, User],
-  logging: false,
 });
 
 module.exports = AppDataSource;
