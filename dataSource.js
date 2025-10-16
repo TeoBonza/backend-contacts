1689e8473d0d9ec4b1b0ad0701386eca6dc1173c
Teo <matteo@peculiarventures.com>
2025-10-16 16:23:58 +0300

    restore env variable DATABASE_CONNECTION_OPTIONS, update code

--- dataSource.js
+++ dataSource.js
@@ -2,15 +2,10 @@ const { DataSource } = require('typeorm');
 const Contact = require('./models/contactModel');
 const User = require('./models/userModel');
 
+const config = JSON.parse(process.env.DATABASE_CONNECTION_OPTIONS);
+
 const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'mycontacts-backend',
-  synchronize: true,
-  logging: process.env.DB_LOGGING === 'true',
+  ...config,
   entities: [Contact, User],
 });
 
